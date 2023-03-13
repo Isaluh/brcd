71736db32d79e020ceb2e4a27aee0fa13f008b5e
Isaluh <82837765+Isaluh@users.noreply.github.com>
2023-03-13 16:33:52 -0300

    Add files via upload

---
+++ player_musica/README.md
@@ -0,0 +1,41 @@
+<h1 align="center"> Music Player </h1>
+
+<p align="center">
+Projeto disponibilizado pela equipe da Rocketseat com intuito de promover layouts para estudo próprio dos participantes.<br/>
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Layout" src="./assets/lt_sofa.png" width="100%">
+</p>
+
+## 🎓 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+<!-- - JavaScript??  --> 
+- Git e Github
+- Figma
+
+## 📔 Projeto
+
+Funciona melhor para os estudantes de objetos em 3D porém com o gif disponibilizado também era uma forma de conclusão. Se baseia em uma página que vende um certo produto mostrando ele em 360°.
+
+- [Visite o projeto online](https://isaluh.github.io/music-player)
+
+## 🏯 Layout
+
+Visualise o layout do projeto através [desse link](https://www.figma.com/file/rIlj6fBhuQTTUNcxH9CSfR/%23boraCodar---Desafio-1-(Copy)?node-id=0%3A1&t=11yv9PVtWu48V5zD-0). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+
+---
+
+<h4 align="center">By: Isaluh 🤍</h4>
---
+++ player_musica/script.js
@@ -0,0 +1,135 @@
+const tocar = document.querySelectorAll("img.play")
+const music = document.querySelector('audio')
+const textoSegundos = document.querySelectorAll('.segundos')
+const textoMinutos = document.querySelectorAll('.minutos')
+const barraUm = document.querySelector('#linhacima')
+const barraDois = document.querySelector('#linhacimadois')
+
+let ativado = 'True'
+let timer = ''
+tocar.forEach((tocarUm) => {
+    tocarUm.addEventListener('click', () => {
+        if (tocarUm.classList.contains('tocando')) {
+            tocar.forEach((tocarUm) => {
+                tocarUm.setAttribute('src', './assets/play.svg')
+                music.pause()
+                tocarUm.classList.remove('tocando')
+            })
+            clearInterval(timer)
+        }
+        else {
+            tocar.forEach((tocarUm) => {
+                tocarUm.setAttribute('src', './assets/pause.svg')
+                music.play()
+                tocarUm.classList.add('tocando')
+            })
+            timer = setInterval(tempoCorrendo, 1000)
+        }             
+    })
+})
+
+let wid = 0
+let widt = 0
+
+function tempoCorrendo() {
+    wid = wid + 0.9178743
+    widt = widt + 1.4492753
+    barraUm.setAttribute('style', `width: ${wid}px`)
+    barraDois.setAttribute('style', `width: ${widt}px`)
+
+    textoSegundos.forEach((segs) =>{
+        segs.innerText = parseInt(segs.innerText) + 1
+        if (segs.innerHTML < 10 && segs.innerHTML > 0) {
+            segs.innerHTML = '0' + segs.innerHTML
+        }
+    })
+
+    if(textoSegundos[0].innerHTML == "60"){
+        textoSegundos.forEach((segs) => {
+            segs.innerText = '00'
+        })
+
+        textoMinutos.forEach((mins) => {
+            mins.innerText = parseInt(mins.innerText) + 1
+            if (mins.innerHTML < 4) {
+                mins.innerHTML = '0' + mins.innerHTML
+            }
+        })
+    }
+    if (textoMinutos[0].innerHTML == '03' && textoSegundos[0].innerHTML == '27') {
+        clearInterval(timer)
+        wid = 0
+        widt = 0
+        barraUm.setAttribute('style', `width: ${wid}px`)
+        barraDois.setAttribute('style', `width: ${widt}px`)
+        textoMinutos.forEach((mins) => {
+            mins.innerHTML = '00'
+        })
+        textoSegundos.forEach((segs) => {
+            segs.innerHTML = '00'
+        })
+        tocar.forEach((tocarUm) => {
+            tocarUm.setAttribute('src', './assets/play.svg')
+            tocarUm.classList.remove('tocando')
+        })
+        music.replay()
+    }
+}
+
+
+
+
+
+// tocar.forEach((parar) => {
+//     parar.addEventListener('click', () => {
+//         if (parar.classList.contains('tocando')) {
+//             tocar.forEach((parar) => {
+//                 parar.setAttribute('src', './assets/play.svg')
+//                 parar.classList.remove('tocando')
+//             })
+//         }
+//     })
+// })
+
+// tocar.forEach((tocarUm) => {
+//     tocarUm.addEventListener('click', () => {
+//         if (tocarUm.classList.contains('play')) {
+//             tocar.forEach((tocarUm) => {
+//                 tocarUm.classList.add('hidden')
+//             })
+//         }
+//     })
+// })
+
+// pausar.forEach((pausarUm) => {
+//     pausarUm.addEventListener('click', () => {
+//         if (pausarUm.classList.contains('pause')) {
+//             pausar.forEach((pausarUm) => {
+//                 pausarUm.classList.add('hidden')
+//                 tocarUm.classList.remove('hidden')
+//             })
+//         }
+//     })
+// })
+
+// const pausar = document.querySelectorAll("img.pause")
+
+// for (const tocarUm of tocar) {
+//     tocarUm.addEventListener('click', () => {
+//         for (const pausarUm of pausar) {
+//             tocarUm.classList.add('hidden')
+//             pausarUm.classList.remove('hidden')
+//         }
+//     })
+// }
+
+// for (const pausarUm of pausar) {
+//     pausarUm.addEventListener('click', () => {
+//         for (const tocarUm of tocar) {
+//             pausarUm.classList.add('hidden')
+//             tocarUm.classList.remove('hidden')
+//         }
+//     })
+// }
+
+
